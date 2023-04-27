67d4f691c5f68d89c4144575e29398e861e6a45a
pawndev <coquelet.c@gmail.com>
2023-04-27 10:10:31 +0200

    feat: Init project

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/andygrunwald/go-jira"
+	"os"
+)
+
+const (
+	JIRA_VERSION_ID = "JIRA_VERSION_ID"
+	JIRA_USERNAME   = "JIRA_USERNAME"
+	JIRA_PASSWORD   = "JIRA_PASSWORD"
+)
+
+func main() {
+	releaseID := os.Getenv(JIRA_VERSION_ID)
+
+	tp := jira.BasicAuthTransport{
+		Username: os.Getenv(JIRA_USERNAME),
+		Password: os.Getenv(JIRA_PASSWORD),
+	}
+	jiraClient, _ := jira.NewClient(tp.Client(), "https://e-tf1.atlassian.net")
+	issues, _, err := jiraClient.Issue.Search(fmt.Sprintf("fixVersion=%s", releaseID), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, issue := range issues {
+		fmt.Printf("- [%s](https://e-tf1.atlassian.net/browse/%s) %s\n", issue.Key, issue.Key, issue.Fields.Summary)
+	}
+}
